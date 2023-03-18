c57884c64beed952c0a9ffb262fb8cf7b8ec5ef7
aavashb <baralaavas@gmail.com>
2023-03-18 12:12:21 +0545

    program shortened

--- count_zeroes_ones_array.asm
+++ count_zeroes_ones_array.asm
@@ -1,27 +1,32 @@
 LXI H,2000H
 MVI C,05H
-MVI D,00H
+
 
 LOOP:MVI B,00H
+	MVI D,00H
 	MVI E,08H 
 	MOV A,M
 	AGAIN: RAR
     	JNC DOWN
         INR B
-        DOWN: DCR E
+        JMP BELOW
+      
+        DOWN: INR D 
+      BELOW:  DCR E
         JNZ AGAIN
-        PUSH H        
+        
+        PUSH H
+        
         MOV A,L
         ADI 05H
         MOV L,A
         MOV M,B
-        MVI A,08H
-        SUB B
-        MOV E,A
+        
         MOV A,L
         ADI 05H
         MOV L,A
-        MOV M,E
+        MOV M,D
+        
         POP H
          INX H
          DCR C
